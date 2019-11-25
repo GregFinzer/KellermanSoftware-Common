fb446c2bae368ef896e7574a45c4cfc38334064b
Greg Finzer <gfinzer@hotmail.com>
2019-11-25 12:51:45 -0500

    Version 1.15  Added UrlCombineSafe

---
+++ KellermanSoftware-Common/WebUtil.cs
@@ -0,0 +1,20 @@
+﻿namespace KellermanSoftware.Common
+{
+    public static class WebUtil
+    {
+        public static string UrlCombineSafe(string baseUrl, string url)
+        {
+            if (string.IsNullOrEmpty(baseUrl))
+                return url;
+
+            string result = string.Format("{0}/{1}", baseUrl.TrimEnd('/'), url.TrimStart('/'));
+
+            if (!result.ToLower().StartsWith("http"))
+            {
+                result = "http://" + result;
+            }
+
+            return result;
+        }
+    }
+}
