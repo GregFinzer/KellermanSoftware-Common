f1154243f128a3673be61dfde2893e6cd4433574
Greg <gfinzer@hotmail.com>
2021-04-30 11:51:42 -0400

    Unit test for InsertSpaces

---
+++ KellermanSoftware.Common.Tests/StringUtilTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KellermanSoftware.Common.Tests
+{
+    [TestFixture]
+    public class StringUtilTests
+    {
+        [Test]
+        public void CanInsertSpaces()
+        {
+            string value = "ThereCanBeOnly1Highlander";
+            string expected = "There Can Be Only 1 Highlander";
+            string result = StringUtil.InsertSpaces(value);
+            Console.WriteLine(result);
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
